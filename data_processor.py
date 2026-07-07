e9cd7220a28e5a7e55ab153febb1396229215fcb
pro4pro32 <patrick.pikell32@gmail.com>
2026-07-07 01:46:49 -0700

    Update data_processor.py

--- data_processor.py
+++ data_processor.py
@@ -17,7 +17,8 @@ from typing import Optional
 import numpy as np
 import pandas as pd
 
-from src.utils import (
+# Płaskie importy
+from utils import (
     BUCKETS, SEASON_GAMES, OF_POSITIONS,
     safe_float, safe_int, normalise_name,
     add_percentile_column,
